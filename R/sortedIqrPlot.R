66996c71846f7fea9137f5519acc6c6c288b1621
Levi Waldron <lwaldron.research@gmail.com>
2011-10-26 19:22:36 -0400

    First complete, warning-free version.

--- R/sortedIqrPlot.R
+++ R/sortedIqrPlot.R
@@ -1,25 +1,25 @@
 sortedIqrPlot <-
-function(lumiobj,batchvar=rep(1,ncol(lumiobj)),dolog2=FALSE,...){
-  if(!length(batchvar)==ncol(lumiobj)) stop("length(batchvar) must be equal to ncol(lumiobj)")
-  if(class(lumiobj)=="LumiBatch")
+function(data.obj,batchvar=rep(1,ncol(data.obj)),dolog2=FALSE,...){
+  if(!length(batchvar)==ncol(data.obj)) stop("length(batchvar) must be equal to ncol(data.obj)")
+  if(class(data.obj)=="LumiBatch")
     {
       library(lumi)
-      lumiobj <- exprs(lumiobj)
-    }else if(class(lumiobj)=="AffyBatch" | class(lumiobj)=="ExpressionSet"){
+      data.obj <- exprs(data.obj)
+    }else if(class(data.obj)=="AffyBatch" | class(data.obj)=="ExpressionSet"){
       library(affy)
-      lumiobj <- exprs(lumiobj)
-    }else if(!class(lumiobj)=="matrix"){
-      stop("lumiobj should be of class LumiBatch, AffyBatch, ExpressionSet, or matrix.")
+      data.obj <- exprs(data.obj)
+    }else if(!class(data.obj)=="matrix"){
+      stop("data.obj should be of class LumiBatch, AffyBatch, ExpressionSet, or matrix.")
     }
   if(dolog2)
     {
-      if(min(lumiobj) <= 0)
+      if(min(data.obj) <= 0)
         {
-          lumiobj <- lumiobj - min(lumiobj) + 1
+          data.obj <- data.obj - min(data.obj) + 1
         }
-      lumiobj <- log2(lumiobj)
+      data.obj <- log2(data.obj)
     }
-  persample.iqr <- apply(lumiobj,2,quantile,probs=c(0.25,0.75),na.rm=TRUE)
+  persample.iqr <- apply(data.obj,2,quantile,probs=c(0.25,0.75),na.rm=TRUE)
   persample.iqr <- persample.iqr[,order(batchvar,apply(persample.iqr,2,diff))]
   plot(1:ncol(persample.iqr),
        seq(min(persample.iqr),max(persample.iqr),length.out=ncol(persample.iqr)),
