20790d23c6ed68ca5ac1f7c7f4c5298e5fb5d644
Levi Waldron <lwaldron.research@gmail.com>
2011-10-26 17:54:48 -0400

    Finished help page for CATplot.R, removed voluminous automatic output from lumibatch.GSE17565.Rd.

--- R/CATplot.R
+++ R/CATplot.R
@@ -1,5 +1,5 @@
 CATplot <-
-function(vec1,vec2,maxrank,make.plot=TRUE,...){ 
+function(vec1,vec2,maxrank=min(length(vec1),length(vec2)),make.plot=TRUE,...){ 
   ##----------------------------------------------------------------------
   ##----------------------------------------------------------------------
   ##Fuction for Concordance at the Top plots
@@ -9,16 +9,26 @@ function(vec1,vec2,maxrank,make.plot=TRUE,...){
   ## make.plot: if TRUE, the plot will be made
   ## ...: arguments passed on to plot
   ##----------------------------------------------------------------------  
-  x1 <- sort(vec1)
-  x2 <- sort(vec2)
-  if(is.na(maxrank)|is.null(maxrank)|maxrank>min(length(x1),length(x2))) maxrank <- min(length(vec1),length(vec2))
+  if(class(vec1)=="numeric" & class(vec2)=="numeric" & !is.null(names(vec1)) & !is.null(names(vec1)))
+    {
+      vec1 <- sort(vec1)
+      vec1 <- names(vec1)
+      vec2 <- sort(vec2)
+      vec2 <- names(vec2)
+    }
+  if(is.na(maxrank) | is.null(maxrank) | maxrank > min(length(vec1),length(vec2)))
+    {
+      maxrank <- min(length(vec1),length(vec2))
+    }
   output <- data.frame(rank=1:maxrank,
-                       fraction=(1:maxrank)/max(length(x1),length(x2)),
                        concordance=NA)
   for (i in 1:nrow(output)){
-    output[i,"concordance"] <- length(intersect(names(x1)[1:i],names(x2)[1:i]))/i
+    output[i,"concordance"] <- length(intersect(vec1[1:i],vec2[1:i]))/i
   }
-  if(make.plot) plot(concordance~rank,data=output,type='l',...)
+  if(make.plot){
+    plot(concordance~rank,data=output,type='l',...)
+    abline(a=0,b=1/min(length(vec1),length(vec2)),lty=2)
+  }
   return(output)
 }
 
